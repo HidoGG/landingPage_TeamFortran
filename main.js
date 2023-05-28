01dc55cf67733fdae60ead68e384cc23641c1ffa
Gabriel Hidalgo <gabriel.hid.orl@gmail.com>
2023-05-27 23:17:27 -0300

    Comentario de código y Funcionalidad
    Se agrego comentarios en código CSS, también, se elimino código innecesarios y se agrego funcionalidad al logo en Js.

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-
-// let ocultoTexto_btn = document.getElementById('ocultoTexto_btn');
-
-// let ocultoTexto = document.getElementById('ocultoTexto');
-
-// ocultoTexto_btn.addEventListener('click', toggleText);
- 
-// function toggleText() {
-    
-//     ocultoTexto.classList.toggle('vicible');
-//     ocultoTexto_btn.classList.remove('vicible');
-    
-// }
-
+/*funcionalidad para ocultar o mostrar un elemento de texto*/
 let ocultoTexto_btn = document.getElementById('ocultoTexto_btn');
 let ocultoTexto = document.getElementById('ocultoTexto');
 
@@ -24,3 +11,11 @@ function toggleText() {
     ocultoTexto_btn.style.display = 'none';
   }
 }
+
+/*funcionalidad de recargar la página cuando se hace clic en el elemento del logo con la clase "logoFortram"*/
+document.addEventListener("DOMContentLoaded", function() {
+  var logo = document.querySelector(".logoFortram");
+  logo.addEventListener("click", function() {
+    location.reload();
+  });
+});
