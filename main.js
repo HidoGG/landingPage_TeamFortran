59db201a1acb18b3f5e288d8d5c632fd28a304ec
Gabriel Hidalgo <88288730+HidoGG@users.noreply.github.com>
2023-06-07 15:34:21 -0300

    Subiendo cambios (#8)

--- main.js
+++ main.js
@@ -19,3 +19,17 @@ document.addEventListener("DOMContentLoaded", function() {
     location.reload();
   });
 });
+
+/*Carusel*/
+window.addEventListener('load', function () {
+  new Glider(document.querySelector('.carousel__lista'), {
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    draggable: true,
+    dots: '.carousel__indicadores',
+    arrows: {
+    prev: '.carousel__anterior',
+    next: '.carousel__siguiente'
+  }
+  });
+})
